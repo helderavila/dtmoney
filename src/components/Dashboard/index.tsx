732351b5bd1dfc106526d5eae28360b276d93d93
Helder Avila <helderavila2016@gmail.com>
2021-03-16 17:44:55 -0300

    feat: Componente de tabela de transações

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -3,11 +3,13 @@ import { Container } from './styles'
 
 // Components
 import { Summary } from '../Summary'
+import { TransactionsTable } from '../TransactionsTable'
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   )
 }
